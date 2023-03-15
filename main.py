5bd3ef878ec427ed04e3d1470095f2bb92a4a313
Patrick <patrickcooper1995@gmail.com>
2023-03-15 19:31:23 -0400

    2023 - first train and test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from utils import data_prep, data_transform
+from utils import data_prep, data_transform, logistic_regression as lr
 
 LOGGER = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
@@ -9,18 +9,27 @@ logging.basicConfig(level=logging.INFO,
                     )
 
 add_external_sources = True
+run_data_setup = False
 
-# SETUP - only needs to be run once to create the database
-data_prep.create_database(add_external_sources)
+if run_data_setup:
+    # SETUP - only needs to be run once to create the database
+    data_prep.create_database(add_external_sources)
 
-# Add advanced stats to DB
-data_prep.create_advanced_statistics()
+    # Add advanced stats to DB
+    data_prep.create_advanced_statistics()
 
-# Standardize team names for external data sources
-data_prep.standardize_team_names()
+    # Standardize team names for external data sources
+    data_prep.standardize_team_names()
 
 # Build regression training data
 data_transform.get_training_data(women=False)
 data_transform.get_test_data(women=False)
+
+# Classify training data
+training_data = lr.classify_data(training=True)
+# And test data
+test_data = lr.classify_data(training=False)
+lr.train_model(training_data)
+lr.test_model(test_data)
 
 LOGGER.info("Done.")
--- utils/logistic_regression.py
+++ utils/logistic_regression.py
@@ -11,29 +11,73 @@ from sklearn.metrics import classification_report, confusion_matrix
 LOGGER = logging.getLogger()
 
 
-def classify_training_data():
+model = LogisticRegression(solver='liblinear', C=10.0, random_state=0)
+
+
+def classify_data(training: bool = True):
     conn = sql.connect("madness.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM TRAINING_SET;")
+
+    table_name = "TRAINING_SET" if training else "TEST_SET"
+    cur.execute(f"SELECT NOR, NDR, NTSP, NFTP, NTOVP FROM {table_name};")
     raw_data = cur.fetchall()
     conn.close()
-    LOGGER.info(f"Retrieved training set (PREVIEW): {raw_data[:10]}")
+    LOGGER.info(f"Retrieved {table_name} set (PREVIEW): {raw_data[:10]}")
 
     LOGGER.info("Classifying data into wins (1) and losses (0)")
 
     training_data = []
     for row in raw_data:
-        # if it's a one, stick with win-focused data
-        print(row)
-        if random.randint(0, 1):
-            training_data.append({1: [value for value in row]})
+        # if it's a 1, stick with win-focused data
+        if table_name == "TRAINING_SET":
+            if random.randint(0, 1):
+                training_data.append({1: [value for value in row]})
+            else:
+                training_data.append({0: [value*-1 for value in row]})
         else:
-            training_data.append({0: [value*-1 for value in row]})
+            training_data.append({1: [value for value in row]})
 
-    print(training_data[:20])
+    return training_data
 
-# x = np.arange(10).reshape(-1, 1)
-# y = np.array([1, 1, 0, 1, 1, 1, 1, 1, 1, 1])
 
-# model = LogisticRegression(solver='liblinear', random_state=0)
-# model.fit(x, y)
+def train_model(training_data: list):
+
+    LOGGER.info("--- Fitting model to training data ---")
+
+    observations = []
+    result = []
+    for obs in training_data:
+        key = [ob for ob in obs][0]
+        observations.append(key)
+        result.append(obs[key])
+    LOGGER.info(f"Data shaped for model training")
+
+    LOGGER.info(f"Beginning logistic regression training for {len(training_data)} observations")
+    model.fit(result, observations)
+    LOGGER.info("Training complete!")
+
+    LOGGER.info(f"Intercept: {model.intercept_}")
+    LOGGER.info(f"Coefficients: {model.coef_}")
+
+
+def test_model(test_data: list):
+    """Test trained model"""
+    LOGGER.info("--- Beginning test of trained model ---")
+    LOGGER.info(f"{len(test_data)} observations to test")
+
+    observations = []
+    for obs in test_data:
+        key = [ob for ob in obs][0]
+        observations.append(obs[key])
+    prob_result = model.predict_proba(observations)
+
+    total = len(prob_result)
+    correct = 0
+    for i in range(len(prob_result)):
+        if prob_result[i][0] > .5:
+            correct += 1
+
+    scoring_array = [1] * len(test_data)
+    LOGGER.info(f"Raw Accuracy: {correct/total}")
+    LOGGER.info(f"Score: {model.score(observations, scoring_array)}")
+    LOGGER.info(f"Classification Report:\n{classification_report(scoring_array, model.predict(observations))}")
